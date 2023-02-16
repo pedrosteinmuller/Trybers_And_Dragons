1e46ba76c63f51739d9ffe2c388b1e7f721f0dbc
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-16 17:36:58 -0300

    feat: feita as configuracoes inicias quando implementado a interface Fighter

---
+++ src/Character.ts
@@ -0,0 +1,40 @@
+import Archetype, { Mage } from './Archetypes';
+import Energy from './Energy';
+import Fighter from './Fighter';
+import Race, { Elf } from './Races';
+import getRandomInt from './utils';
+
+export default class Character implements Fighter {
+  private _race: Race;
+  private _archetype: Archetype;
+  private _maxLifePoints: number;
+  private _lifePoints: number;
+  private _strength: number;
+  private _defense: number;
+  private _dexterity: number;
+  private _energy: Energy;
+
+  constructor(name: string) {
+    this._dexterity = getRandomInt(1, 10);
+    this._race = new Elf(name, this._dexterity);
+    this._archetype = new Mage(name);
+    this._maxLifePoints = (this._race.maxLifePoints / 2);
+    this._lifePoints = this._maxLifePoints;
+    this._strength = getRandomInt(1, 10);
+    this._defense = getRandomInt(1, 10);
+    this._energy = {
+      type_: this._archetype.energyType,
+      amount: getRandomInt(1, 10),
+    };
+  }
+
+  get race(): Race { return this._race; }
+  get archetype(): Archetype { return this._archetype; }
+  get lifePoints(): number { return this._lifePoints; }
+  get maxLifePoints(): number { return this._maxLifePoints; }
+  get strength(): number { return this._strength; }
+  get defense(): number { return this._defense; }
+  get dexterity(): number { return this._dexterity; }
+  get energy(): Energy { return { ...this._energy }; }
+
+}
