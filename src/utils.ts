77f01dd3c4ecaceac4dfed03da15d1a933205b48
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-16 14:20:45 -0300

    feat: first commit

--- src/utils.ts
+++ src/utils.ts
@@ -3,5 +3,5 @@ function getRandomInt(min: number, max: number) {
   const newMax = Math.floor(max);
   return Math.floor(Math.random() * (newMax - newMin)) + newMin;
 }
-
+// first commit
 export default getRandomInt;
