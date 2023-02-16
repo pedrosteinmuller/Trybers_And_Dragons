70e57c8ebf299c2ab32b3b29e88347d1242fe5a3
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-16 17:10:22 -0300

    feat: criada interface Fighter

---
+++ src/Fighter/Fighter.ts
@@ -0,0 +1,15 @@
+import Energy from '../Energy';
+
+interface Fighter {
+  lifePoints: number;
+  strength: number;
+  defense: number;
+  energy?: Energy;
+
+  attack(enemy: Fighter): void;
+  special?(enemy: Fighter): void;
+  levelUp(): void;
+  receiveDamage(attackPoints: number): number;
+}
+
+export default Fighter;
