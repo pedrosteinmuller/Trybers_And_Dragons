33007dfbfa5d63214badb91bc780192f3eb5420f
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-17 15:21:42 -0300

    feat: criado os objetos para simular batalhas

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import Battle, { PVE, PVP } from './Battle';
+import Character from './Character';
+import Dragon from './Dragon';
+import Monster from './Monster';
+
+const player1 = new Character('Pedro');
+const player2 = new Character('Pedro Steinmuller');
+const player3 = new Character('Pedro Steinmuller Pimentel');
+
+player1.levelUp();
+player1.levelUp();
+
+const monster1 = new Monster();
+const monster2 = new Dragon();
+
+const pvp = new PVP(player2, player3);
+
+const pve = new PVE(player1, [monster1, monster2]);
+
+const runBattles = (battles: Battle[]) => {
+  const finalResult = battles.map((battle: Battle) => battle.fight());
+  console.log((finalResult));
+};
+
+export {
+  player1,
+  player2,
+  player3,
+  monster1,
+  monster2,
+  pvp,
+  pve,
+  runBattles,
+};
