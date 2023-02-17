932f6e41776989362010f58128ead64b43b866d5
pedrosteinmuller <pedrostein@DESKTOP-TR5PH8G.localdomain>
2023-02-17 16:28:56 -0300

    fix: adicionando metodo attack na interface Fighter

--- src/Fighter/Fighter.ts
+++ src/Fighter/Fighter.ts
@@ -1,11 +1,11 @@
 import Energy from '../Energy';
 import SimpleFighter from './SimpleFighter';
 
-interface Fighter extends SimpleFighter {
+export default interface Fighter extends SimpleFighter {
   defense: number;
   energy?: Energy;
+
+  attack(enemy: Fighter | SimpleFighter): void;
   special?(enemy: Fighter): void;
   levelUp(): void;
 }
-
-export default Fighter;
